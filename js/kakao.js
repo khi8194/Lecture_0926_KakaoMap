e400355a5f187847d015b98db8c63ee5f63de645
khi8194 <khi8194@gmail.com>
2024-09-26 17:48:04 +0900

    지도 리사이즈시 맵컨테이너 안쪽에 엘리먼트 요소 중첩 수정, 리사이즈시 버튼도 같이 초기화

--- js/kakao.js
+++ js/kakao.js
@@ -4,7 +4,7 @@ const btnToggle = document.querySelector(".trafficToggle");
 
 const mapOptions = {
 	center: new kakao.maps.LatLng(37.5803593, 127.0042622),
-	level: 3
+	level: 5
 };
 
 //스크립트가 처음 로드된 시점에 mapOption안에 포함되어 있는 위치값을 기준으로 지도 인스턴스가 생성되고 끝
@@ -16,20 +16,33 @@ const mapOptions = {
 //브라우저 리사이즈 될때마다 map변수에 변경된 값을 재반영해야 되므로 let 방식으로 변수 선언
 let map = new kakao.maps.Map(mapContainer, mapOptions);
 let marker = new kakao.maps.Marker({ position: mapOptions.center });
+const mapTypeControl = new kakao.maps.MapTypeControl();
+const zoomControl = new kakao.maps.ZoomControl();
 marker.setMap(map);
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 //브라우저 리사이즈 이벤트가 발생할때마다
 window.addEventListener("resize", () => {
+	//리사이즈 될때마다 중첩되는 엘리먼트 요소들을 일단은 지워서 초기화
+	mapContainer.innerHTML = "";
 	//기존 map, marker 변수에 변경된 인스턴스 정보값을 덮어쓰기 처리
 	map = new kakao.maps.Map(mapContainer, mapOptions);
 	marker.setMap(map);
+	//리사이즈 될때마다 컨트롤 패널 다시 추가
+	map.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
+	map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+	//리사이즈시 강제 토글버튼 초기화
+	btnToggle.classList.remove("on");
+	btnToggle.innerText = "Traffic ON";
 });
 
-const mapTypeControl = new kakao.maps.MapTypeControl();
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
+// const mapTypeControl = new kakao.maps.MapTypeControl();
+// map.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
 
-const zoomControl = new kakao.maps.ZoomControl();
-map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+// const zoomControl = new kakao.maps.ZoomControl();
+// map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 지도에 교통정보를 표시하도록 지도타입을 추가합니다
 //지도 인스턴스에 교통량 정보 레이어 추가
