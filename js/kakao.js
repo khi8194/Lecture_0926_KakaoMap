09f2491277a30e551c5bfb83a7f4b064637f5044
khi8194 <khi8194@gmail.com>
2024-09-26 16:51:32 +0900

    교통량정보를 토글기능으로 변경

--- js/kakao.js
+++ js/kakao.js
@@ -1,4 +1,6 @@
 const mapContainer = document.querySelector("#map");
+// const [btnOn, btnOff] = document.querySelectorAll("nav button");
+const btnToggle = document.querySelector(".trafficToggle");
 
 const mapOptions = {
 	center: new kakao.maps.LatLng(37.5803593, 127.0042622),
@@ -10,18 +12,33 @@ const marker = new kakao.maps.Marker({ position: mapOptions.center });
 
 marker.setMap(map);
 
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-//맵 위에 올릴 타입 컨트롤 인스턴스 생성
 const mapTypeControl = new kakao.maps.MapTypeControl();
-
-// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-//타입컨트롤러의 위치값 지정하는 함수
-//위성지도 타입 변경 컨트롤러 추가 (BOTTOMRIGHT TOPLEFT TOPRIGHT)
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
 
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-//맵 위에 올릴 줌 컨트롤 인트턴스 생성
 const zoomControl = new kakao.maps.ZoomControl();
-//줌 컨트롤러의 위치값 지정하는 함수
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+//지도 인스턴스에 교통량 정보 레이어 추가
+// map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+// 아래 코드는 위에서 추가한 교통정보 지도타입을 제거합니다
+// map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+//미션1- traffic ON 버튼 클릭 시 교통량 레이어 활성화, traffic OFF 버튼 클릭시 교통량 레이어 비활성화
+//미션2- 토글버튼 : 버튼 하나로 위의 기능을 껐다 켰다 하도록 설정
+
+// btnOn.addEventListener("click", () => map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC));
+// btnOff.addEventListener("click", () => map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC));
+
+btnToggle.addEventListener("click", e => {
+	e.target.classList.toggle("on");
+
+	if (e.target.classList.contains("on")) {
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		e.target.innerText = "Traffic OFF";
+	} else {
+		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		e.target.innerText = "Traffic ON";
+	}
+});
